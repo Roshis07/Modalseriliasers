6af07617027810ad552e088afd1734d5de4435c7
Roshis <roshis.tamang@gmail.com>
2023-11-01 11:03:46 +1100

    update with CRUD

--- newApi/models.py
+++ newApi/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 class User(models.Model):
     name = models.CharField(max_length=10)
-    number = models.IntegerField(max_length=10)
+    number = models.IntegerField()
     address = models.CharField(max_length=20)
 
     def __str__(self):
--- newApi/serializers.py
+++ newApi/serializers.py
@@ -1,8 +1,22 @@
 from rest_framework import serializers
+from .models import User
 
 
-class userSerializer(serializers.Serializer):
-    id = serializers.ImageField(read_only=True)
-    name = serializers.StringRelatedField()
-    number = serializers.StringRelatedField()
-    address = serializers.StringRelatedField()
+class UserSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    name = serializers.CharField()
+    number = serializers.IntegerField()
+    address = serializers.CharField()
+
+    def create(self, validated_data):
+        return User.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        # Change this line to update the instance fields
+        instance.name = validated_data.get('name', instance.name)
+        instance.number = validated_data.get('number', instance.number)
+        instance.address = validated_data.get('address', instance.address)
+
+        # Save the instance to persist the changes
+        instance.save()
+        return instance
